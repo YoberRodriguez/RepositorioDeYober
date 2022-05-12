592cf39001dc0a96365747358052486dd0548ad5
Yober Wilmer Rodriguez Lujan <N00182492@upn.pe>
2022-05-12 13:00:07 -0500

    Segunda actualizacion

--- Ejemplo1/Program.cs
+++ Ejemplo1/Program.cs
@@ -1,16 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ejemplo1
+namespace ejercicio
 {
-    internal class Program
+    class ejercicioYober
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.ReadLine("BIENVENIDO");
+            float[] lado = new float[3];
+            float mayor = 0, sumar = 0;
+            int i, j, auxi = 0, flag = 0;
+
+            for (i = 0; i < 3; i++)
+            {
+                Console.Write("Lado {0}:", i + 1);
+                lado[i] = float.Parse(Console.ReadLine());
+
+                if (lado[i] > mayor)
+                {
+                    mayor = lado[i];
+                    auxi = i;
+                }
+            }
+
+            for (i = 0; i < 3; i++)
+                if (auxi != i)
+                    sumar += lado[i];
+
+            if (mayor < sumar)
+            {
+                Console.Write("Es un triangulo");
+
+                for (i = 0; i < 2; i++)
+                    for (j = i + 1; j < 3; j++)
+                        if (lado[i] == lado[j])
+                            flag++;
+                if (flag == 3)
+                    Console.Write(" ---> Equilatero");
+                else
+                {
+                    flag = 0;
+                    for (i = 0; i < 2; i++)
+                        for (j = i + 1; j < 3; j++)
+                            if (lado[i] != lado[j])
+                                flag++;
+                    if (flag == 3)
+                        Console.Write(" ---> Escaleno");
+                    else
+                        Console.Write(" ---> Isosceles");
+                }
+            }
+            Console.Read();
         }
     }
 }
